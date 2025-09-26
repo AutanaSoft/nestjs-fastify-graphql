549d68cffb60782036d947a5a3a7649bd6de1065
AutanaSoft <admin@autanasoft.com>
2025-09-26 06:30:16 -0400

    refactor(user): reorganizar propiedades de entidad de usuario y agregar índice único

--- src/modules/users/domain/entities/user.entity.ts
+++ src/modules/users/domain/entities/user.entity.ts
@@ -1,24 +1,26 @@
+import { HashUtils } from '@shared/applications/utils/hash.utils';
 import {
   BeforeInsert,
   BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { HashUtils } from '@shared/applications/utils/hash.utils';
 import { UserRole, UserStatus } from '../enums/user.enum';
 
 @Entity('users')
+@Index(['email', 'userName'], { unique: true })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true, length: 64 })
+  @Column({ length: 64 })
   email: string;
 
-  @Column({ unique: true, length: 20 })
+  @Column({ name: 'user_name', length: 20 })
   userName: string;
 
   @Column({ length: 64 })
@@ -30,10 +32,10 @@ export class UserEntity {
   @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
   role: UserRole;
 
-  @CreateDateColumn()
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
-  @UpdateDateColumn()
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 
   @BeforeInsert()
